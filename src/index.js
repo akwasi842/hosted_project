0915c55de098b8b1b8dc4f13f12f80aa35f222ed
akwasiaboagye <saboagy5@gmail.com>
2022-10-02 17:11:15 -0700

    updated

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById('ezekiel_clothing'));
 root.render(
   <React.StrictMode>
     <App />
