be533389f69942725af9df1157800c0c92b779f7
akwasiaboagye <saboagy5@gmail.com>
2022-10-17 13:21:50 -0800

    updated

--- src/views/Contact.js
+++ src/views/Contact.js
@@ -49,7 +49,7 @@ const Contact = () => {
                 <div class="col-lg-6 wow fadeInUp" data-wow-delay="0.1s" style={{minHeight: "450px"}}>
                     <div class="position-relative h-100">
                         <iframe class="position-relative w-100 h-100"
-                        src="https://www.google.com/maps/embed?pb=!1m10!1m8!1m3!1d369.1640320570288!2d-0.29669545587352103!3d5.601659467614071!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sen!2sgh!4v1659379501635!5m2!1sen!2sgh" frameborder="0" style={{minHeight: "450px", border:"0"}} allowfullscreen="" aria-hidden="false"
+                        src="https://www.google.com/maps/embed?pb=!1m10!1m8!1m3!1d6123.761663442982!2d-0.29615772849209654!3d5.6012270724189905!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sen!2sgh!4v1665998476588!5m2!1sen!2sgh" frameborder="0" style={{minHeight: "450px", border:"0"}} allowfullscreen="" aria-hidden="false"
                         tabindex="0" title='map' ></iframe>
                     </div>
                 </div>
